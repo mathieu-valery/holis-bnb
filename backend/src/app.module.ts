7917dd01ac1b2b240bcb891c01409ffb65df4108
Paul J <paul.jonquet@holis.com>
2022-06-13 12:19:59 +0200

    feat: remove ormconfig.json file

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -5,7 +5,22 @@ import { AppService } from './app.service';
 import { LocationModule } from './modules/locations/Location.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(), LocationModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      name: 'default',
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'docker',
+      entities: ['dist/**/*.entity{ .ts,.js}'],
+      synchronize: true,
+      migrations: ['dist/migrations/*{.ts,.js}'],
+      migrationsTableName: 'migrations_typeorm',
+      migrationsRun: true,
+    }),
+    LocationModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
