0c5ba3ef9b1e85e23fea129ac44f5b9756b10c0e
Mathieu VALERY <66632538+mathieu-valery@users.noreply.github.com>
2023-01-18 21:59:06 +0100

    fetch locations to state

--- frontend/src/pages/Search/Search.tsx
+++ frontend/src/pages/Search/Search.tsx
@@ -1,11 +1,28 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './Search.css';
+import axios from 'axios';
 
 type SearchPageProps = {};
+const url = 'localhost:8000';
 
 const SearchPage: React.FC<SearchPageProps> = () => {
   // Create a function to fetch all locations from database
+  const [locations, setLocations] = useState(null);
 
+  async function fetchLocations() {
+    const { data } = await axios.get(`http://${url}/locations`);
+    return data;
+  }
+
+  useEffect(() => {
+    fetchLocations().then((res) => {
+      setLocations(res);
+    });
+  }, []);
+
+  useEffect(() => {
+    console.log(locations);
+  }, [locations]);
   // Create a function to sort locations by categories & by number of rooms
 
   // Create a search function linked to the search input in the header
