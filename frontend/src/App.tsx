584e3716b18d0b3e534f6c24f587dd178fb148e6
Mathieu VALERY <66632538+mathieu-valery@users.noreply.github.com>
2023-01-21 22:13:03 +0100

    Index page connected

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import SearchPage from './pages/Search/Search';
+import DisplayLocation from './pages/DisplayLocation/DisplayLocation';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Layout from './layouts/Layout';
 import { LocationsProvider } from './components/Context/LocationsContext';
@@ -11,6 +12,7 @@ function App() {
         <Routes>
           <Route element={<Layout />}>
             <Route index element={<SearchPage />} />
+            <Route path="/locations/:id" element={<DisplayLocation />} />
           </Route>
         </Routes>
       </LocationsProvider>
--- frontend/src/pages/DisplayLocation/DisplayLocation.tsx
+++ frontend/src/pages/DisplayLocation/DisplayLocation.tsx
@@ -1,28 +1,66 @@
-import React from 'react';
-
+import React, { useEffect, useState } from 'react';
+import { useLocation } from 'react-router-dom';
+import { fetchLocation } from '../../helpers/ajax';
 type DisplayLocationPageProps = {};
+
+interface Category {
+  id: number;
+  name: string;
+  description: string;
+}
+interface Location {
+  id: number;
+  title: string;
+  description: string;
+  location: string;
+  picture: string;
+  stars: number;
+  numberOfRooms: number;
+  price: number;
+  categoryId: number;
+  category: Category;
+}
 
 const DisplayLocationPage: React.FC<DisplayLocationPageProps> = () => {
   // Create a function to handle price change and persist it to database
-
   // Create a function to delete the location and persist it to database
+  // Confusing because the state is named 'location
+  const CurrentLocationObject = useLocation();
+  const regex = /\d/g;
+  let id: string;
+  console.log(CurrentLocationObject.pathname.match(regex));
+  const matched: RegExpMatchArray | null = CurrentLocationObject.pathname.match(regex);
+  if (matched) {
+    id = matched[0];
+  }
+
+  const [location, setLocation] = useState<Location | null>(null);
+
+  useEffect(() => {
+    fetchLocation(id).then((res) => {
+      setLocation(res);
+    });
+  }, []);
 
   return (
     <div className="display-location">
-      {/* image */}
-
-      <div className="display-location__content">
-        {/* title */}
-        {/* property type */}
-        {/* category description */}
-        {/* price */}
-      </div>
-
-      <div className="display-location__edit">
-        {/* price input */}
-        {/* price button */}
-        {/* delete button */}
-      </div>
+      {location && (
+        <>
+          <h1>{location.title}</h1>
+          {/* image */}
+          <div className="display-location__content">
+            {/* title */}
+            {/* property type */}
+            {/* category description */}
+            {/* price */}
+          </div>
+          <div className="display-location__edit">
+            {/* price input */}
+            {/* price button */}
+            {/* delete button */}
+          </div>
+        </>
+      )}
     </div>
   );
 };
