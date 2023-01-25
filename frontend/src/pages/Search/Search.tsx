fb4c1add82d7ed53bbdeae30dda00651b2108106
Mathieu VALERY <66632538+mathieu-valery@users.noreply.github.com>
2023-01-25 19:37:26 +0100

    put locationsByCategoriesAndNbrOfRooms in useMemo

--- frontend/src/pages/Search/Search.tsx
+++ frontend/src/pages/Search/Search.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useContext } from 'react';
+import React, { useEffect, useContext, useMemo } from 'react';
 import './Search.css';
 import { Location } from '../../helpers/interfaces';
 import Card from '../../components/Card/Card';
@@ -7,7 +7,6 @@ import LocationsContext from '../../components/Context/LocationsContext';
 type SearchPageProps = {};
 
 const SearchPage: React.FC<SearchPageProps> = () => {
-  let locationsByCategoriesAndNbrOfRooms: any = [];
   const locationsContext: any = useContext(LocationsContext);
   const locations = locationsContext.locations;
 
@@ -15,12 +14,12 @@ const SearchPage: React.FC<SearchPageProps> = () => {
     locationsContext.getLocations();
   }, []);
 
-  let groupByCategories = (array: any) => {
+  const groupByCategories = (array: any) => {
     return array.reduce((acc: any, currentLocation: Location) => {
       const findedCategoryIndex = acc.findIndex(
         (category: any) => category.id === currentLocation.category.id
       );
-      if (findedCategoryIndex > 0) {
+      if (findedCategoryIndex > -1) {
         acc[findedCategoryIndex].locations.push(currentLocation);
       } else {
         acc.push({ ...currentLocation.category, locations: [currentLocation] });
@@ -29,7 +28,7 @@ const SearchPage: React.FC<SearchPageProps> = () => {
     }, []);
   };
 
-  let groupByNbrOfRooms = (array: any) => {
+  const groupByNbrOfRooms = (array: any) => {
     return array.reduce((acc: any, currentLocation: any) => {
       const findedNumberOfRoomsIndex = acc.findIndex(
         (el: any) => el.numberOfRooms === currentLocation.numberOfRooms
@@ -43,37 +42,44 @@ const SearchPage: React.FC<SearchPageProps> = () => {
     }, []);
   };
 
-  if (locations) {
-    locationsByCategoriesAndNbrOfRooms = groupByCategories(locations);
-    locationsByCategoriesAndNbrOfRooms.forEach(
-      (el: any) => (el.locations = groupByNbrOfRooms(el.locations))
-    );
-    locationsByCategoriesAndNbrOfRooms.map((category: any) =>
-      category.locations.sort((a: any, b: any) => a.numberOfRooms - b.numberOfRooms)
-    );
-  }
+  const filterByCategorieAndNumberOfRooms = (locations: any) => {
+    if (locations) {
+      const locationsByCategories = groupByCategories(locations);
+      locationsByCategories.forEach((el: any) => (el.locations = groupByNbrOfRooms(el.locations)));
+      locationsByCategories.map((category: any) =>
+        category.locations.sort((a: any, b: any) => a.numberOfRooms - b.numberOfRooms)
+      );
+      return locationsByCategories;
+    }
+  };
+
+  const locationsByCategoriesAndNbrOfRooms: any = useMemo(
+    () => filterByCategorieAndNumberOfRooms(locations),
+    [locations]
+  );
 
   return (
     <div className="search">
-      {locationsByCategoriesAndNbrOfRooms.map((category: any) => (
-        <React.Fragment key={category.id}>
-          <h1 key={category.id} className="category-title">
-            {category.name}
-          </h1>
-          {category.locations.map((obj: any, index: string) => (
-            <div key={index} className="rooms-section">
-              <h2 className="rooms-title">
-                {`${obj.numberOfRooms} room${obj.numberOfRooms > 1 ? 's' : ''}`}
-              </h2>
-              <div className="cards-section">
-                {obj.locations.map((location: Location) => (
-                  <Card key={location.id} location={location}></Card>
-                ))}
+      {locationsByCategoriesAndNbrOfRooms &&
+        locationsByCategoriesAndNbrOfRooms.map((category: any) => (
+          <React.Fragment key={category.id}>
+            <h1 key={category.id} className="category-title">
+              {category.name}
+            </h1>
+            {category.locations.map((obj: any, index: string) => (
+              <div key={index} className="rooms-section">
+                <h2 className="rooms-title">
+                  {`${obj.numberOfRooms} room${obj.numberOfRooms > 1 ? 's' : ''}`}
+                </h2>
+                <div className="cards-section">
+                  {obj.locations.map((location: Location) => (
+                    <Card key={location.id} location={location}></Card>
+                  ))}
+                </div>
               </div>
-            </div>
-          ))}
-        </React.Fragment>
-      ))}
+            ))}
+          </React.Fragment>
+        ))}
     </div>
   );
 };
